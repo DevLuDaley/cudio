cc51bd208e1510c2d4b75e9887ff81a31fd31e4a
lhsharing <daley.luis@gmail.com>
2019-05-17 18:15:43 -0400

    deletions

--- app/controllers/CallbacksController.rb
+++
@@ -1,8 +0,0 @@
-class CallbacksController < Devise::OmniauthCallbacksController 
-
-  def github
-    @engineer = Engineer.from_omniauth(request.env["omniauth.auth"])
-    signin_and_redirect @engineer
-  end
-
-end
