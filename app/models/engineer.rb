a79f14b8f7b61a8783ca0cf79902a376b9ba00d5
lhsharing <daley.luis@gmail.com>
2019-05-17 15:49:25 -0400

    + #devise :omniauthable

---
+++ app/models/engineer.rb
@@ -0,0 +1,9 @@
+class Engineer < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+#:recoverable, :rememberable, :validatable
+#devise :database_authenticatable, :registerable, 
+:recoverable, :rememberable, :trackable, :validatable #, :omniauthable
+  #devise :omniauthable
+end
