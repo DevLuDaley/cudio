1a36a73f3ea0ddde54119812a7d0610d8a4d15c7
lhsharing <daley.luis@gmail.com>
2019-05-17 16:54:55 -0400

    +devise auth to Engineers table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_17_200415) do
+ActiveRecord::Schema.define(version: 2019_05_17_205344) do
 
   create_table "artists", force: :cascade do |t|
     t.string "name"
@@ -27,6 +27,8 @@ ActiveRecord::Schema.define(version: 2019_05_17_200415) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "provider"
+    t.string "uid"
     t.index ["email"], name: "index_engineers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_engineers_on_reset_password_token", unique: true
   end
