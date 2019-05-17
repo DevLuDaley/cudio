dba896d837444198938e8abf746f26f7b16e2929
lhsharing <daley.luis@gmail.com>
2019-05-17 18:15:33 -0400

    *routes* update devise_for engineers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :engineers
+#  devise_for :engineers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+
+  devise_for :engineers, :controllers => { omniauth_callbacks: "engineers/omniauth_callbacks", :registrations => "registrations" }
+
+
+
 end
