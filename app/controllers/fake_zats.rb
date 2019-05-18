601dbb170937765ac8872b67049047129991d9cc
lhsharing <daley.luis@gmail.com>
2019-05-18 00:24:08 -0400

    fake zats controller used to view code

---
+++ app/controllers/fake_zats.rb
@@ -0,0 +1,21 @@
+class FakeZatsController < Devise::RegistrationsController
+
+#     def index
+#         @zats = Zat.all
+#     end
+# # def new
+#     @zat = Zat.new
+# end
+
+# def show
+# @zat = Zat.find(params[:id])
+# end
+
+# def create
+#     @zat = Zat.create(appointment_date: params[appointment_date],
+#                       engineer: engineers[3], 
+#                       studio: studios[3], 
+#                       artist: artists[3])
+# end
+
+end
