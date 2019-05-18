39435d04d5d9044d1d91a367f6c262a5aca84738
lhsharing <daley.luis@gmail.com>
2019-05-18 00:24:31 -0400

    added artist/zats/studios resources + zats/index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
+  get 'zats/index'
 #  devise_for :engineers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+resources :artists #, :videos
+resources :studios do
+  resources :zats
+end
+
+#get 'engineers/:id/zats/:zats_id'
 
 
   devise_for :engineers, :controllers => { omniauth_callbacks: "engineers/omniauth_callbacks", :registrations => "registrations" }
