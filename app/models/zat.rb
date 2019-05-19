c337dcfc52a325c65c7bbd7de4d50d8cb53ec137
lhsharing <daley.luis@gmail.com>
2019-05-19 04:44:11 -0400

    +complete!|? and incomplete!|?

--- app/models/zat.rb
+++ app/models/zat.rb
@@ -3,5 +3,37 @@ class Zat < ApplicationRecord
   belongs_to :engineer
   belongs_to :artist
   belongs_to :studio
-  
+
+
+STATUS = {
+        :incomplete => 0,
+        :complete => 1
+    }
+    #use hash above to set up key 10:00 v3
+    #clearly define meaning of #0 || #1 in context
+    def complete?
+        self.status == STATUS[:complete]    
+        #self.status == 1
+    end
+
+    def incomplete?
+        self.status == STATUS[:incomplete]
+        #self.status == 0
+    end
+
+    def complete!
+        self.status = STATUS[:complete]
+        self.save
+        #self.status == 0
+    end
+
+    def incomplete!
+        self.status = STATUS[:incomplete]
+        self.save
+        #self.status == 0
+    end
+
+
+
+
 end
