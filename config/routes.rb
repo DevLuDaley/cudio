f6cbf1cbdf4584e6a57d5692d9238072a93352ec
lhsharing <daley.luis@gmail.com>
2019-05-19 13:34:45 -0400

    +zats/edit and other routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,17 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-  get 'zats/index'
-#  devise_for :engineers
+  #  devise_for :engineers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-get 'zats/:id/edit', to: 'zats#edit', as: :edit_zat
-patch 'posts/:id', to: 'posts#update'
+  get 'zats/index'
+  get 'zats/:id/edit', to: 'zats#edit', as: :edit_zat
+  patch 'zats/:id', to: 'zats#update'
+  resources :zats
 
-resources :artists #, :videos
-resources :studios
-resources :zats
+  resources :artists # , :videos
+  resources :studios
 
-#get 'engineers/:id/zats/:zats_id'
+  # get 'engineers/:id/zats/:zats_id'
 
-
-  devise_for :engineers, :controllers => { omniauth_callbacks: "engineers/omniauth_callbacks", :registrations => "registrations" }
-
-
-
+  devise_for :engineers, controllers: { omniauth_callbacks: 'engineers/omniauth_callbacks', registrations: 'registrations' }
 end
