235d1634dc88ed76d5556ab3cda8200e3825981c
lhsharing <daley.luis@gmail.com>
2019-05-19 04:46:35 -0400

    +'zats/:id/edit' + patch 'posts/:id' route.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,12 @@ Rails.application.routes.draw do
   get 'zats/index'
 #  devise_for :engineers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+get 'zats/:id/edit', to: 'zats#edit', as: :edit_zat
+patch 'posts/:id', to: 'posts#update'
 
 resources :artists #, :videos
-resources :studios do
-  resources :zats
-end
+resources :studios
+resources :zats
 
 #get 'engineers/:id/zats/:zats_id'
 
