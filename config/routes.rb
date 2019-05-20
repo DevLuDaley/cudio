e02a9495b31579852664434a93abdae57ce8e29b
lhsharing <daley.luis@gmail.com>
2019-05-20 17:12:26 -0400

    clean-up routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,24 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'zats/index'
-  #  devise_for :engineers
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'zats/:id/edit', to: 'zats#edit', as: :edit_zat
-  patch 'zats/:id', to: 'zats#update'
-
+  resources :zats
   resources :artists # , :videos
   resources :studios
-  resources :zats
-
-  # get 'engineers/:id/zats/:zats_id'
-
   devise_for :engineers, controllers: { omniauth_callbacks: 'engineers/omniauth_callbacks', registrations: 'registrations' }
 
-  resources :engineers do
-    resources :zats
-  end
+  resources :engineers
+
+  #  get 'zats/:id', to: 'zats#show', as: 'session'
+  # get 'zats/index'
+  #  devise_for :engineers
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # get 'zats/:id/edit', to: 'zats#edit', as: :edit_zat
+  # patch 'zats/:id', to: 'zats#update'
+
+  #  get 'studios/index'
+  # get 'engineers/:id/zats/:zats_id'
+
+  # resources :engineers do
+  #   resources :zats
+  #  end
 end
