255a4c54a7e6f72329a4db9cd18110ad8e22e838
lhsharing <daley.luis@gmail.com>
2019-05-20 09:05:22 -0400

    cleaned-up routes.rb routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,20 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  # get 'engineers/index'
-  # get 'studios/index'
-  # get 'artists/index'
+  get 'zats/index'
   #  devise_for :engineers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'zats/index'
   get 'zats/:id/edit', to: 'zats#edit', as: :edit_zat
   patch 'zats/:id', to: 'zats#update'
-  # post 'zats/new', to: 'zats#new'
-  get 'engineers/:id', to: 'engineers#show', as: 'engineer'
 
-  resources :zats
   resources :artists # , :videos
   resources :studios
+  resources :zats
 
   # get 'engineers/:id/zats/:zats_id'
+
   devise_for :engineers, controllers: { omniauth_callbacks: 'engineers/omniauth_callbacks', registrations: 'registrations' }
+
   resources :engineers do
     resources :zats
   end
