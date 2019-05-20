5d031d051c57da68a9dd6f6ad6d335766d9ffe97
lhsharing <daley.luis@gmail.com>
2019-05-19 20:20:19 -0400

    troubleshooting

--- app/helpers/zat_helper.rb
+++ app/helpers/zat_helper.rb
@@ -1,2 +1,7 @@
+# frozen_string_literal: true
+
 module ZatHelper
+  #   def display_zat(engineer)
+  #     engineer.zat.nil? ? link_to('Add Zat', edit_zat_path(engineer)) : link_to(engineer.zat_name, zat_path(engineer.zat))
+  #   end
 end
