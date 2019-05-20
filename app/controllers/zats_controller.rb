ad77cf912b4901d72b258bf54f087445324b670e
lhsharing <daley.luis@gmail.com>
2019-05-19 20:20:08 -0400

    +new params +_id to each model

--- app/controllers/zats_controller.rb
+++ app/controllers/zats_controller.rb
@@ -14,20 +14,22 @@ class ZatsController < ApplicationController
 
   def new
     @zat = Zat.new
+    # params.require(:zat).permit(:appointment_date, :studio_id, :engineer_id, :artist_id)
+
     index
   end
 
   def zat_params
-    params.require(:zat).permit(:appointment_date, :studio, :engineer, :artist)
+    params.require(:zat).permit(:appointment_date, :studio_id, :engineer_id, :artist_id)
   end
 
   def create
     @zat = Zat.new(zat_params)
-
+    index
     if @zat.save
-      redirect_to action: 'index'
+      redirect_to action: 'index', notice: 'Recording Session created'
     else
-      @studios = Studio.all
+      # @studios = Studio.all
       render action: 'new'
     end
   end
@@ -37,25 +39,51 @@ class ZatsController < ApplicationController
     index
   end
 
-  def zat_param
-    params.require(:zat).permit(:appointment_date, :studio, :engineer, :artist)
-  end
+  #   def zat_param
+  #     params.require(:zat).permit(:appointment_date, :studio_id, :engineer_id, :artist_id)
+  #   end
+
+  # def update
+  # @zat = Zat.find(params[:id])
+  # @zat.update(params.require(:zat).permit(:appointment_date, :studio, :engineer, :artist)
+  # raise params.inspect
+  # if @zat.update_attributes(zat_param)
+  #    redirect_to action: 'show', id: @zat
+  #  else
+
+  #    @zats = Zat.all
+  #    @engineers = Engineer.all
+  #    @engineer = Engineer.find(params[:id])
+  #    @studios = Studio.all
+  #    @artists = Artist.all
+  #    render action: 'edit'
+  # end
+  # end
 
   def update
+    index
     @zat = Zat.find(params[:id])
     # raise params.inspect
-    # if @zat.update_attributes(zat_param)
-    redirect_to action: 'show', id: @zat
-    # else
-
-    @zats = Zat.all
-    @engineers = Engineer.all
-    @engineer = Engineer.find(params[:id])
-    @studios = Studio.all
-    @artists = Artist.all
-    #  render action: 'edit'
-    # end
+    @zat.update(params.require(:zat).permit(:appointment_date, :studio_id, :engineer_id, :artist_id))
+    redirect_to zat_path(@zat)
   end
+
+  #   def update
+  #     @zat = Zat.find(params[:id])
+  #     #@zat.update(params.require(:zat).permit(:appointment_date, :studio, :engineer, :artist)
+  #     raise params.inspect
+  #     if @zat.update_attributes(zat_param)
+  #       redirect_to action: 'show', id: @zat
+  #     else
+
+  #       @zats = Zat.all
+  #       @engineers = Engineer.all
+  #       @engineer = Engineer.find(params[:id])
+  #       @studios = Studio.all
+  #       @artists = Artist.all
+  #       render action: 'edit'
+  #     end
+  #   end
 
   def delete
     Zat.find(params[:id]).destroy
