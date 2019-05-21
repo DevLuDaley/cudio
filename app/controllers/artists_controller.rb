747e48a74781f0a80ef8a3cd6222595bacd838e6
lhsharing <daley.luis@gmail.com>
2019-05-21 09:20:28 -0400

    +logic to ArtistsCtrl

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,4 +1,27 @@
-class ArtistController < ApplicationController
+class ArtistsController < ApplicationController
   def index
+    @artists = Artist.all
   end
+
+
+  def edit
+    @artist = Artist.find(params[:id])
+ #   index
+  end
+
+def update
+#    index
+    @artist = Artist.find(params[:id])
+    # raise params.inspect
+    if @artist.update(params.require(:artist).permit(:artist_name))
+      # binding.pry
+      redirect_to artist_path(@artist)
+
+    else
+      render 'edit'
+  end
+
+
+end
+
 end
