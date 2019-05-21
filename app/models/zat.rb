98bb4cc410d62ad4b94ee914a392a76a5fd37849
lhsharing <daley.luis@gmail.com>
2019-05-21 16:11:49 -0400

    added validations to zat.rb

--- app/models/zat.rb
+++ app/models/zat.rb
@@ -9,6 +9,17 @@ STATUS = {
         :incomplete => 0,
         :complete => 1
     }
+
+
+validates :engineer_id, presence: true, length: { minimum: 1 }
+validates :artist_id, presence: true, length: { minimum: 1 }
+validates :studio_id, presence: true, length: { minimum: 1 }
+
+#use the validation below to ensure that an appointment/session date is entered
+#validates :appointment_date, presence: true, length: { minimum: 1 }
+#validates :appointment_date, uniqueness: true
+
+
     #use hash above to set up key 10:00 v3
     #clearly define meaning of #0 || #1 in context
     def complete?
