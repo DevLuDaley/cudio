186ffed1560a99f86d9a6a0f3c47f8be4960506f
lhsharing <daley.luis@gmail.com>
2019-05-21 16:08:09 -0400

    +valiations to artists.rb

--- app/models/studio.rb
+++ app/models/studio.rb
@@ -4,4 +4,12 @@ class Studio < ApplicationRecord
           has_many :engineers, through: :zats
           has_many :artists, through: :zats
 
+validates :name, presence: true, length: { minimum: 1 }
+validates :name, uniqueness: true
+
+validates :location, presence: true, length: { minimum: 1 }
+
+
+
+
 end
