b00c52be2af14bab42629095ae78ced29a76f77f
lhsharing <daley.luis@gmail.com>
2019-05-21 22:21:06 -0400

    + index + show to Engineer ctrl

---
+++ app/controllers/engineers_controller.rb
@@ -0,0 +1,23 @@
+class EngineersController < ApplicationController
+#class EngineersController < Devise::RegistrationsController
+
+
+  def index
+    @engineers = Engineer.all
+    @zats = Zat.all
+    @studios = Studio.all
+    @artists = Artist.all
+   #    redirect_to action: 'index'
+ end
+
+
+def show
+    index
+    # binding.pry
+    @engineer = Engineer.find(params[:id])
+    #     redirect_to action: 'index'
+    
+  end
+
+
+end
