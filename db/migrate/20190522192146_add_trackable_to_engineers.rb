ea18d6d183d0bf7ba42af758e762bf6d13a28e1f
lhsharing <daley.luis@gmail.com>
2019-05-22 19:35:36 -0400

    +migration table for 6 missed devise columns

---
+++ db/migrate/20190522192146_add_trackable_to_engineers.rb
@@ -0,0 +1,13 @@
+class AddTrackableToEngineers < ActiveRecord::Migration[5.2]
+  def change
+    add_column :engineers, :sign_in_count, :integer, default: 0, null: false
+    add_column :engineers, :current_sign_in_at, :datetime
+    add_column :engineers, :last_sign_in_at, :datetime
+    add_column :engineers, :current_sign_in_ip, :string
+    add_column :engineers, :last_sign_in_ip, :string
+
+
+#copied rom the engineers file.
+
+  end
+end
