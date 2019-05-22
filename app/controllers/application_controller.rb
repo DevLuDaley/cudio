62c6bef74a1912cb2058ad225aae99b309cf997e
lhsharing <daley.luis@gmail.com>
2019-05-22 19:29:20 -0400

    +config params & destroy method to app ctrl

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,33 @@
 class ApplicationController < ActionController::Base
-   
+  #index
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email])
+  end
+
+
+  def destroy
+ #   binding.pry
+        # Engineer.find(session[:engineer_id]).destroy
+      #  session_cookie.destroy
+      sign_out @engineer
+#           redirect_to '/'
+new_engineer_session_path
+#devise_scope :engineer do
+  #  get '/signout', to: 'devise/sessions#destroy', as: :signout
+ # end
+# destroy_engineer_session_url
+#redirect_to destroy_engineer_session_path, method: :delete
+        #    redirect_to '/engineer
+end
+
+
+
+
+
+
 end
